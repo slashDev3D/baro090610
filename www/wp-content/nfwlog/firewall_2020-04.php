fa36bc7bedbb620a7dc109f56c9d84f7e52c4ff5
slash <dev@slash.builders>
2023-01-13 17:27:53 +0900

    archiving

---
+++ www/wp-content/nfwlog/firewall_2020-04.php
@@ -0,0 +1,6 @@
+<?php exit; ?>
+[1587286506] [0] [baro090610.cafe24.com] [#5540645] [0] [6] [121.143.103.117] [403] [POST] [/wp-admin/update.php] [Plugin uploaded by baro090610] [hex:4e616d653a20656c656d656e746f722d70726f2d322e342e382e7a6970]
+[1587286600] [0] [baro090610.cafe24.com] [#3579932] [0] [6] [121.143.103.117] [403] [GET] [/wp-admin/update.php] [Plugin installed by baro090610] [hex:4e616d653a20656c656d656e746f72]
+[1587286797] [0] [baro090610.cafe24.com] [#8822454] [0] [6] [121.143.103.117] [403] [POST] [/wp-admin/update.php] [Plugin uploaded by baro090610] [hex:4e616d653a206b626f6172642d636f6d6d656e74732d776f726470726573732d706c7567696e2d342e362e7a6970]
+[1587286817] [0] [baro090610.cafe24.com] [#2749067] [0] [6] [121.143.103.117] [403] [POST] [/wp-admin/update.php] [Plugin uploaded by baro090610] [hex:4e616d653a206b626f6172642d776f726470726573732d706c7567696e2d352e352e7a6970]
+[1587313804] [0] [baro090610.cafe24.com] [#2669068] [0] [6] [0.0.0.0] [403] [N/A] [-] [Database changes detected] [hex:61646d696e6973747261746f72206163636f756e74]
